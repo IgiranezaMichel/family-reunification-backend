fac2a1d6289f34480aeccb2ad43ac1448be6eadd
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-31 19:37:32 +0200

    create table

---
+++ src/main/java/com/familyreunificationbackend/repository/GroupMember.java
@@ -0,0 +1,7 @@
+package com.familyreunificationbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupMember extends JpaRepository<GroupMember,Long>{
+
+}
---
+++ src/main/java/com/familyreunificationbackend/repository/ReceiverRepository.java
@@ -0,0 +1,9 @@
+package com.familyreunificationbackend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.familyreunificationbackend.model.Receiver;
+
+public interface ReceiverRepository extends JpaRepository<Receiver,Long>{
+
+}
