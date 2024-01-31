7b937b8a4c0ae83ff5d00fa866abdb6348138dbc
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-01-31 19:34:15 +0200

    Create Case Table

---
+++ src/main/java/com/familyreunificationbackend/model/Case.java
@@ -0,0 +1,21 @@
+package com.familyreunificationbackend.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Case {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+private long id;
+private String name;
+private String description;
+private LocalDateTime timeStamp;
+}
