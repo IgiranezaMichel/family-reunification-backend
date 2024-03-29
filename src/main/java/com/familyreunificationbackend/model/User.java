8cc8b6e34cc34d2f06fa5d34b7dedc65fc7d02ab
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-29 10:44:27 +0200

    add user

--- src/main/java/com/familyreunificationbackend/model/User.java
+++ src/main/java/com/familyreunificationbackend/model/User.java
@@ -1,6 +1,12 @@
 package com.familyreunificationbackend.model;
 
 import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +14,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class User {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
 private long id;
 private String firstName;
 private String lastName;
+@Column(columnDefinition = "longBlob")
+private String profilePicture;
 private String gender;
 private String email;
 private LocalDate dob;
