fb1c0a314feaaf28c2e95fc7a4085ea04d1e7efd
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-29 10:44:18 +0200

    add location

--- src/main/java/com/familyreunificationbackend/model/Location.java
+++ src/main/java/com/familyreunificationbackend/model/Location.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,6 +20,8 @@ public class Location {
 private long id;
 private String name;
 private String type;
+@ManyToOne()
 private Location location;
+@OneToMany(mappedBy = "location")
 private List<Location>locationList;
 }
