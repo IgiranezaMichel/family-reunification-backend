8d8663b931e576b37c59ea9e7ae6dc1b4e09b103
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-29 16:10:43 +0200

    add organization services

--- src/main/java/com/familyreunificationbackend/services/OrganizationServices.java
+++ src/main/java/com/familyreunificationbackend/services/OrganizationServices.java
@@ -1,11 +1,39 @@
 package com.familyreunificationbackend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.familyreunificationbackend.model.Organization;
+import com.familyreunificationbackend.model.pagination.OrganizationPage;
+import com.familyreunificationbackend.model.paginationDefinition.PaginationInput;
 import com.familyreunificationbackend.repository.OrganizationRepository;
 
 @Service
 public class OrganizationServices {
     @Autowired private OrganizationRepository organizationRepository;
+    public ResponseEntity<String>saveOrUpdateOrganization(Organization data){
+        Organization org=organizationRepository.save(data);
+        return new ResponseEntity<>(org.getName()+" saved successful",HttpStatus.OK);
+    }
+    public Organization findOrganizationById(long id){
+        return organizationRepository.findById(id).orElseThrow();
+    }
+    public ResponseEntity<String> deleteOrganization(long id){
+        try {
+            Organization org=this.findOrganizationById(id);
+            organizationRepository.delete(org);
+            return new ResponseEntity<>(org.getName()+" removed successful",HttpStatus.OK);
+        } catch (Exception e) {
+           return new ResponseEntity<>("Organization not found",HttpStatus.METHOD_NOT_ALLOWED);
+        } 
+    }
+    public OrganizationPage organizationPage(PaginationInput page){
+        Page<Organization>pagination=organizationRepository.findAll(PageRequest.of(page.getPageNumber(),page.getPageSize(),Sort.by(page.getSort())));
+        return new OrganizationPage(pagination.getNumber(), page.getPageSize(), pagination.getTotalElements(), pagination.getContent());
+    }
 }
