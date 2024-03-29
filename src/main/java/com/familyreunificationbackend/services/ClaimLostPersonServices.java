07cb21c5a048de31210353e4182c63755007a7e7
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-03-29 15:44:45 +0200

    add claimLostPersonServices

--- src/main/java/com/familyreunificationbackend/services/ClaimLostPersonServices.java
+++ src/main/java/com/familyreunificationbackend/services/ClaimLostPersonServices.java
@@ -1,11 +1,18 @@
 package com.familyreunificationbackend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.familyreunificationbackend.model.User;
 import com.familyreunificationbackend.repository.ClaimLostPersonRepository;
 
 @Service
 public class ClaimLostPersonServices {
     @Autowired private ClaimLostPersonRepository claimLostPersonRepository;
+    public ResponseEntity<String> saveOrUpdate(User user){
+        claimLostPersonRepository.save(null);
+        return new ResponseEntity<>("",HttpStatus.OK);
+    }
 }
