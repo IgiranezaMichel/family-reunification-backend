2f93ec818d1e76339601c8486aeb9dc1d07bd994
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-01 10:58:27 +0200

    add pagination

--- src/main/java/com/familyreunificationbackend/services/UserServices.java
+++ src/main/java/com/familyreunificationbackend/services/UserServices.java
@@ -1,10 +1,44 @@
 package com.familyreunificationbackend.services;
 
+import java.util.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.familyreunificationbackend.enums.Role;
+import com.familyreunificationbackend.input.UserInput;
+import com.familyreunificationbackend.model.User;
+import com.familyreunificationbackend.model.pagination.UserPage;
+import com.familyreunificationbackend.model.paginationDefinition.PaginationInput;
 import com.familyreunificationbackend.repository.UserRepository;
 @Service
 public class UserServices {
     @Autowired private UserRepository userRepository;
+    public ResponseEntity<String> saveOrUpdateUser(UserInput userInput){
+        User user=new User(userInput.getId(), userInput.getFirstName(), userInput.getLastName(), Base64.getDecoder().decode(userInput.getBase64ProfilePicture()), userInput.getGender(), userInput.getEmail(), userInput.getPhoneNumber(), userInput.getDob(), userInput.getAddress(), userInput.getCountry(), userInput.getNativeCountry(), Role.USER, userInput.getUsername(), userInput.getPassword());
+        User result=userRepository.save(user);
+        if(userInput.getId()!=0)return new ResponseEntity<>("Hi "+result.getFirstName()+" Your Information has updated successful",HttpStatus.OK);
+        return new ResponseEntity<>("Hi "+result.getFirstName()+" Your Information has saved successful",HttpStatus.OK); 
+    }
+    public User findUserById(long id){
+        return userRepository.findById(id).orElseThrow();
+    }
+    @SuppressWarnings("null")
+    public ResponseEntity<String> deleteUser(long id){
+        try {
+         User user=this.findUserById(id);
+         userRepository.delete(user);
+         return new ResponseEntity<>(user.getFirstName()+" has deleted successful",HttpStatus.OK);
+        } catch (Exception e) {
+           return new ResponseEntity<>("User not found",HttpStatus.METHOD_NOT_ALLOWED);
+        }
+    }
+    public UserPage userPage(PaginationInput page){
+        Page<User>pagination=userRepository.findAll(PageRequest.of(page.getPageNumber(),page.getPageSize(),Sort.by(page.getSort())));
+        return new UserPage(pagination.getNumber(), page.getPageSize(), pagination.getTotalElements(), pagination.getContent());
+    }
 }
