b036cbfca278df5056d0758fdae4ca2b819d2494
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-01 10:57:24 +0200

    add controller functionality

---
+++ src/main/java/com/familyreunificationbackend/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.familyreunificationbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import com.familyreunificationbackend.input.UserInput;
+import com.familyreunificationbackend.model.pagination.UserPage;
+import com.familyreunificationbackend.model.paginationDefinition.PaginationInput;
+import com.familyreunificationbackend.services.UserServices;
+
+@Controller
+public class UserController {
+    @Autowired
+    private UserServices userServices;
+
+    @MutationMapping()
+    public ResponseEntity<String> saveUser(@Argument(name = "userInput") UserInput userInput) {
+        return userServices.saveOrUpdateUser(userInput);
+    }
+
+    @MutationMapping()
+    public ResponseEntity<String> deleteUser(@Argument(name = "id") long id) {
+        return userServices.deleteUser(id);
+    }
+    @QueryMapping()
+    public UserPage userPage(@Argument(name = "page")PaginationInput paginationInput){
+        return userServices.userPage(paginationInput);
+    }
+}
