da4978c838af533c41155dc3d11499a0d1fce76a
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-01 09:34:23 +0200

    remove role functionality

--- src/main/java/com/familyreunificationbackend/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.familyreunificationbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.familyreunificationbackend.model.Role;
-
-public interface RoleRepository extends JpaRepository<Role,Long>{
-
-}
