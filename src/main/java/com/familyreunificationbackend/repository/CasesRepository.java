cdc04741d8ec14db74719c1bf27fac9cdda567b0
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 18:13:25 +0200

    create existsByTitle

--- src/main/java/com/familyreunificationbackend/repository/CasesRepository.java
+++ src/main/java/com/familyreunificationbackend/repository/CasesRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.familyreunificationbackend.model.Cases;
 public interface CasesRepository extends JpaRepository<Cases,Long>{
 
+    boolean existsByTitle(String title);
+
 }
