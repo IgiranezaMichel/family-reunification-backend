46cf533401b9085cc47a9edeffdb8b242956bc88
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 17:56:44 +0200

    add constructor

--- src/main/java/com/familyreunificationbackend/model/User.java
+++ src/main/java/com/familyreunificationbackend/model/User.java
@@ -2,9 +2,11 @@ package com.familyreunificationbackend.model;
 
 import java.time.LocalDate;
 import java.util.Base64;
+import java.util.List;
 
 import com.familyreunificationbackend.enums.Role;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -13,6 +15,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -46,4 +49,24 @@ public class User {
     public String getProfilePicture(){
         return Base64.getEncoder().encodeToString(profilePicture);
     }
+    public User(long id,String firstName,String lastName,byte[] profilePicture, String gender,
+   String email, String phoneNumber, LocalDate dob, String address, String country, String nativeCountry
+    ,Role role, String username, String password){
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.profilePicture=profilePicture;
+        this.gender=gender;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+        this.dob=dob;
+        this.address=address;
+        this.country=country;
+        this.nativeCountry=nativeCountry;
+        this.role=role;
+        this.username=username;
+        this.password=password;
+    }
+    @OneToMany(mappedBy = "user",targetEntity=Cases.class,cascade =CascadeType.ALL )
+    public List<Cases>caseList;
 }
