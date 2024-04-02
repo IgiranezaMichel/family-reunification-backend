82391314de4bead0496793b1e00e032c296a4676
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 17:57:15 +0200

    import Cases

---
+++ src/main/java/com/familyreunificationbackend/repository/CasesRepository.java
@@ -0,0 +1,7 @@
+package com.familyreunificationbackend.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.familyreunificationbackend.model.Cases;
+public interface CasesRepository extends JpaRepository<Cases,Long>{
+
+}
