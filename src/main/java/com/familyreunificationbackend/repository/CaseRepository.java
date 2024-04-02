3a6adbf9b4da4e54e048a6d04bfdf26765500dc6
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 17:57:03 +0200

    rename to caseRepository

--- src/main/java/com/familyreunificationbackend/repository/CaseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.familyreunificationbackend.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.familyreunificationbackend.model.UserCase;
-
-public interface CaseRepository extends JpaRepository<UserCase,Long>{
-
-}
