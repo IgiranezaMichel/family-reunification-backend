7431648ade86273558ad92885e4263eeb385eb34
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 18:22:34 +0200

    add warning

--- src/main/java/com/familyreunificationbackend/configurations/CorsConfiguration.java
+++ src/main/java/com/familyreunificationbackend/configurations/CorsConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  
