db16db724d48e391bb4bf0d42d0df4e22080b13a
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-02 17:56:13 +0200

    create case controller

---
+++ src/main/java/com/familyreunificationbackend/controller/CaseController.java
@@ -0,0 +1,34 @@
+package com.familyreunificationbackend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import com.familyreunificationbackend.model.Cases;
+import com.familyreunificationbackend.services.CaseServices;
+
+@Controller
+public class CaseController {
+    @Autowired
+    private CaseServices caseServices;
+
+    @MutationMapping
+    public ResponseEntity<String> saveCases(@Argument(name = "caseInput") Cases cases,@Argument(name = "userId")long userId) {
+        return caseServices.saveOrUpdate(cases,userId);
+    }
+
+    @MutationMapping
+    public ResponseEntity<String> deleteCases(@Argument(name = "id") long id) {
+        return caseServices.deleteCase(id);
+    }
+
+    @QueryMapping
+    public List<Cases> caseList() {
+        return caseServices.caseList();
+    }
+}
