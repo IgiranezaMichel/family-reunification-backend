062bc0ccb58f6305cae6da89ef65a35186f04e4c
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-04 18:39:18 +0200

    add timestamp and it pattern

--- src/main/java/com/familyreunificationbackend/model/Cases.java
+++ src/main/java/com/familyreunificationbackend/model/Cases.java
@@ -1,6 +1,6 @@
 package com.familyreunificationbackend.model;
 import com.familyreunificationbackend.enums.Role;
-
+import java.time.format.DateTimeFormatter;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,6 +10,7 @@ import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,4 +24,9 @@ private String description;
 @ManyToOne(cascade = CascadeType.ALL,targetEntity =Customer.class)
 private Customer customer;
 private Role role;
+private LocalDateTime timeStamp;
+public String getTimeStamp(){
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss a");
+    return formatter.format(timeStamp);
+}
 }
