168947b6f05f5d1711c0cd108eef3ee305556873
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-04 18:40:21 +0200

    import getTimeStamp

--- src/main/java/com/familyreunificationbackend/model/Organization.java
+++ src/main/java/com/familyreunificationbackend/model/Organization.java
@@ -1,23 +1,41 @@
 package com.familyreunificationbackend.model;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Lob;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Organization {
 @Id @GeneratedValue(strategy = GenerationType.AUTO)
 private long id;
 private String name;
-private String logo;
+@Lob
+private byte[] logo;
 private String description;
 private String address;
 private LocalDateTime timeStamp;
+public String getTimeStamp(){
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss a");
+    return formatter.format(timeStamp);
+}
+public String getLogo(){
+    return "data:image/png;base64,"+Base64.getEncoder().encodeToString(logo);
+}
+public Organization(long id,String name,String logo,String description,String address){
+this.id=id;
+this.name=name;
+String image=logo.split("base64,")[1];
+this.logo=Base64.getDecoder().decode(image);
+this.description=description;
+this.address=address;
+this.timeStamp=LocalDateTime.now();
+}
 }
