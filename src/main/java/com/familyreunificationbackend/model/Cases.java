ceaea12fc85e8beda0eef98a854c2cf6bb47c61b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-06 21:19:36 +0200

    create cases

--- src/main/java/com/familyreunificationbackend/model/Cases.java
+++ src/main/java/com/familyreunificationbackend/model/Cases.java
@@ -1,12 +1,15 @@
 package com.familyreunificationbackend.model;
 import com.familyreunificationbackend.enums.Role;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,6 +28,8 @@ private String description;
 private Customer customer;
 private Role role;
 private LocalDateTime timeStamp;
+@OneToMany(cascade =CascadeType.ALL,targetEntity =Lost.class)
+private List<Lost>lostCases;
 public String getTimeStamp(){
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss a");
     return formatter.format(timeStamp);
