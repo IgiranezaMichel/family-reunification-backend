38fbb7e43ac011d652354ec29c0f5dd1215fd202
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-06 21:19:46 +0200

    add document

--- src/main/java/com/familyreunificationbackend/model/Document.java
+++ src/main/java/com/familyreunificationbackend/model/Document.java
@@ -2,12 +2,16 @@ package com.familyreunificationbackend.model;
 
 import java.util.UUID;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.annotations.UuidGenerator.Style;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,4 +28,7 @@ private String fileName;
 private String encodingName;
 @Lob
 private byte [] file;
+@ManyToOne(cascade = CascadeType.ALL,targetEntity=Lost.class)
+@OnDelete(action = OnDeleteAction.CASCADE)
+private Lost lostDocument;
 }
