84444ee70b08e8f4a92fea658318d61465e39600
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-23 21:07:27 +0200

    add uuid

--- src/main/java/com/familyreunificationbackend/controller/LostController.java
+++ src/main/java/com/familyreunificationbackend/controller/LostController.java
@@ -1,5 +1,7 @@
 package com.familyreunificationbackend.controller;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
@@ -12,7 +14,11 @@ import com.familyreunificationbackend.services.LostServices;
 public class LostController {
 @Autowired private LostServices lostServices;
 @MutationMapping
-public ResponseEntity<String>saveLost(@Argument LostDTO lostDTO){
+public ResponseEntity<String>saveLost(@Argument(name = "lost") LostDTO lostDTO){
     return lostServices.saveLost(lostDTO);
+}
+@MutationMapping
+public ResponseEntity<String>deleteLost(@Argument(name = "id")UUID lostId){
+    return lostServices.deleteLost(lostId);
 }
 }
