d13d4474fc3736eae413ea5b720b41f8d0940449
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-24 14:48:55 +0200

    formatting

--- src/main/java/com/familyreunificationbackend/services/DocumentServices.java
+++ src/main/java/com/familyreunificationbackend/services/DocumentServices.java
@@ -13,16 +13,16 @@ public class DocumentServices {
     @Autowired
     private DocumentRepository documentRepository;
 
-public ResponseEntity<String> saveOrUpdateDocument(DocumentInput document){
-    try {
-        byte[] decodeDocument=Base64.getDecoder().decode(document.getBase64Document().split("base64,")[1]);
-        document.setEncodingName(document.getBase64Document().split("base64,")[0]+"base64,");
-        document.setFile(decodeDocument);
-        Document doc=documentRepository.save(document);
-        return new ResponseEntity<>(doc.getFileName()+" File has been added successful",HttpStatus.OK);
+    public ResponseEntity<String> saveOrUpdateDocument(DocumentInput document) {
+        try {
+            byte[] decodeDocument = Base64.getDecoder().decode(document.getBase64Document().split("base64,")[1]);
+            document.setEncodingName(document.getBase64Document().split("base64,")[0] + "base64,");
+            document.setFile(decodeDocument);
+            Document doc = documentRepository.save(document);
+            return new ResponseEntity<>(doc.getFileName() + " File has been added successful", HttpStatus.OK);
 
-    } catch (Exception e) {
-return new ResponseEntity<>("Unable to store uploaded file",HttpStatus.METHOD_NOT_ALLOWED);
-}
-}
+        } catch (Exception e) {
+            return new ResponseEntity<>("Unable to store uploaded file", HttpStatus.METHOD_NOT_ALLOWED);
+        }
+    }
 }
