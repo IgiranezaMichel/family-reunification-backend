4321b400ed8ce882dd76d93abd55b68c96b5361b
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-30 17:39:32 +0200

    add sort

--- src/main/java/com/familyreunificationbackend/repository/LostRepository.java
+++ src/main/java/com/familyreunificationbackend/repository/LostRepository.java
@@ -1,6 +1,7 @@
 package com.familyreunificationbackend.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -17,5 +18,6 @@ public interface LostRepository extends JpaRepository<Lost,UUID>{
             PageRequest page);
     @Query("Select l from Lost l where l.hasFound=:hasFound and CAST(l.timeStamp AS DATE)>=:fromDate and cast(l.timeStamp AS DATE)<=:toDate or l.countryOfLost=:countryOfLost")
     Page<Lost> findAllByLostFilter(boolean hasFound, String countryOfLost,LocalDate fromDate,LocalDate toDate, PageRequest page);
+    List<Lost> findAllByPostedByAndHasFound(Customer customer, boolean hasFound, Sort by);
 
 }
