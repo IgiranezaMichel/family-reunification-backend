e15d9ad2913216f783ce9811700ab81eaed065b5
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-30 15:45:10 +0200

    add findby id

--- src/main/java/com/familyreunificationbackend/services/LostServices.java
+++ src/main/java/com/familyreunificationbackend/services/LostServices.java
@@ -1,14 +1,19 @@
 package com.familyreunificationbackend.services;
 
+import java.time.LocalDate;
 import java.util.Base64;
 import java.util.List;
 import java.util.UUID;
 import org.hibernate.bytecode.enhance.spi.EnhancementException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.familyreunificationbackend.dto.LostDTO;
+import com.familyreunificationbackend.dto.LostPageDTO;
+import com.familyreunificationbackend.dto.LostPageInput;
 import com.familyreunificationbackend.model.Cases;
 import com.familyreunificationbackend.model.Customer;
 import com.familyreunificationbackend.model.Lost;
@@ -41,7 +46,7 @@ public class LostServices {
         }
     }
 
-    public Lost findById(UUID id) {
+    public Lost findById(UUID id){
         return lostRepository.findById(id).orElseThrow(() -> new EnhancementException("Lost not found"));
     }
 
@@ -57,5 +62,18 @@ public class LostServices {
 
     public List<Lost> findAll() {
         return lostRepository.findAll();
+    }
+    public List<Lost>getCustomerLostPosts(long customerId){
+        Customer customer=customerServices.findCustomerById(customerId);
+        return lostRepository.findAllByPostedBy(customer);
+    }
+    public LostPageDTO<Lost> lostPageable(LostPageInput input){
+        var page=PageRequest.of(input.getPageNumber(), input.getPageSize(), Sort.by(input.getSort()));
+        if(input.getToDate()==null)input.setToDate(LocalDate.now());
+        if(input.getFromDate()==null)input.setFromDate(LocalDate.now());
+        if(input.getFromDate().isAfter(LocalDate.now()))input.setFromDate(LocalDate.now());
+        if(input.getFromDate().isAfter(input.getToDate()))input.setToDate(LocalDate.now());
+        var paginationResult=lostRepository.findAllByLostFilter(input.getHasFound(),input.getCountry(),input.getFromDate(),input.getToDate(),page);
+        return new  LostPageDTO<>(paginationResult.getNumber(), paginationResult.getTotalPages(), paginationResult.getTotalElements(), paginationResult.getContent());
     }
 }
