9d14a802a90c4f625b6057793b8fec28db57ae04
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-05-30 15:44:29 +0200

    add find all by posted by

--- src/main/java/com/familyreunificationbackend/repository/LostRepository.java
+++ src/main/java/com/familyreunificationbackend/repository/LostRepository.java
@@ -1,7 +1,21 @@
 package com.familyreunificationbackend.repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.familyreunificationbackend.model.Customer;
 import com.familyreunificationbackend.model.Lost;
+
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 public interface LostRepository extends JpaRepository<Lost,UUID>{
+
+    List<Lost> findAllByPostedBy(Customer customer);
+    Object findAllByHasFoundAndCountryOfLostAndTimeStamp(boolean hasFound, String country, LocalDate fromDate,
+            PageRequest page);
+    @Query("Select l from Lost l where l.hasFound=:hasFound and CAST(l.timeStamp AS DATE)>=:fromDate and cast(l.timeStamp AS DATE)<=:toDate or l.countryOfLost=:countryOfLost")
+    Page<Lost> findAllByLostFilter(boolean hasFound, String countryOfLost,LocalDate fromDate,LocalDate toDate, PageRequest page);
 
 }
