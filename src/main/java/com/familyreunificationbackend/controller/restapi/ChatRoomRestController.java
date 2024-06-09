31d87c8d19dc7dc2718e7477c115a22cdfab70c2
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-10 01:28:45 +0200

    ChatRoom Rest Controller

--- src/main/java/com/familyreunificationbackend/controller/restapi/ChatRoomRestController.java
+++ src/main/java/com/familyreunificationbackend/controller/restapi/ChatRoomRestController.java
@@ -9,11 +9,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.familyreunificationbackend.dto.ChatListDTO;
 import com.familyreunificationbackend.dto.ChatRoomDTO;
 import com.familyreunificationbackend.model.ChatRoom;
 import com.familyreunificationbackend.restbodyinput.ChatRoomRBI;
 import com.familyreunificationbackend.services.ChatRoomServices;
+import com.familyreunificationbackend.services.CustomerServices;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,15 +28,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class ChatRoomRestController {
     @Autowired private ChatRoomServices chatRoomServices;
+    @Autowired private CustomerServices customerServices;
 @PostMapping(value = "/create")
 public ResponseEntity<ChatRoomDTO> ChatMessage(@RequestBody ChatRoomRBI chatRoomDTO) {
     ChatRoom chatRoom= chatRoomServices.createChatRoom(chatRoomDTO);
     return new ResponseEntity<>(new ChatRoomDTO(chatRoom.getId(), chatRoom.getChatInitiator().getUsername(),chatRoom.getChatInitiator().getFirstName()+" "+chatRoom.getChatInitiator().getLastName(),chatRoom.getChatInitiator().getProfilePicture(), chatRoom.getChatSubscriber().getUsername(),chatRoom.getChatSubscriber().getProfilePicture(),chatRoom.getChatSubscriber().getFirstName()+" "+chatRoom.getChatSubscriber().getLastName()),HttpStatus.OK);
 }
 @GetMapping("/all")
-public List<ChatRoomDTO> getAllChatRooms(Principal principal) {
+public List<ChatRoomDTO> getAllChatRooms(Principal principal,@RequestParam String search) {
     log.info("Username ============ {}",principal.getName());
-       return chatRoomServices.getAllUserChatRooms(principal.getName());
+       return chatRoomServices.getAllUserChatRooms(principal.getName(),search);
 }
-
+@GetMapping("user/all")
+public List<ChatListDTO>getAllCustomer(){
+    return customerServices.getAllCutomerToChatList();
+}
 }
