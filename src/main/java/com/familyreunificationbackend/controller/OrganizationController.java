877ecf6bdcb55adb7f72028f94064a20d9265350
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-10 01:28:21 +0200

    Organization Controller

--- src/main/java/com/familyreunificationbackend/controller/OrganizationController.java
+++ src/main/java/com/familyreunificationbackend/controller/OrganizationController.java
@@ -1,5 +1,7 @@
 package com.familyreunificationbackend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import com.familyreunificationbackend.input.OrganizationInput;
+import com.familyreunificationbackend.model.Organization;
 import com.familyreunificationbackend.model.pagination.OrganizationPage;
 import com.familyreunificationbackend.model.paginationDefinition.PaginationInput;
 import com.familyreunificationbackend.services.OrganizationServices;
@@ -32,4 +35,9 @@ public class OrganizationController {
     public OrganizationPage organizationPage(@Argument(name = "organizationPageInput") PaginationInput input) {
         return organizationServices.organizationPage(input);
     }
+    @QueryMapping()
+    public List<Organization> organizationList() {
+        return organizationServices.organizationList();
+    }
+    
 }
