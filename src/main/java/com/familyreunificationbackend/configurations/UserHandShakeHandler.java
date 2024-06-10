abbf783362c498cdc6515c8eb048d614eb914ab2
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-10 21:02:40 +0200

    add hand shake handler

---
+++ src/main/java/com/familyreunificationbackend/configurations/UserHandShakeHandler.java
@@ -0,0 +1,24 @@
+package com.familyreunificationbackend.configurations;
+
+import java.security.Principal;
+import java.util.Map;
+
+import org.apache.catalina.connector.CoyotePrincipal;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.lang.Nullable;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class UserHandShakeHandler extends DefaultHandshakeHandler{
+
+    @SuppressWarnings("null")
+    @Override
+    @Nullable
+    protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler,
+            Map<String, Object> attributes) {
+                log.info("session : ",request.getHeaders());
+        return new CoyotePrincipal(request.getPrincipal().getName());
+    }
+
+}
