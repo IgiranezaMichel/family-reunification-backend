c25cef08bf4322d407c08771bf7fd9fe913a09e1
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-13 08:37:59 +0200

    LostRepository

--- src/main/java/com/familyreunificationbackend/repository/LostRepository.java
+++ src/main/java/com/familyreunificationbackend/repository/LostRepository.java
@@ -4,15 +4,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import com.familyreunificationbackend.model.Customer;
 import com.familyreunificationbackend.model.Lost;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 public interface LostRepository extends JpaRepository<Lost,UUID>{
-
     List<Lost> findAllByPostedBy(Customer customer);
     Object findAllByHasFoundAndCountryOfLostAndTimeStamp(boolean hasFound, String country, LocalDate fromDate,
             PageRequest page);
@@ -20,6 +17,22 @@ public interface LostRepository extends JpaRepository<Lost,UUID>{
     Page<Lost> findAllByLostFilter(boolean hasFound, String countryOfLost,LocalDate fromDate,LocalDate toDate, PageRequest page);
     List<Lost> findAllByPostedByAndHasFound(Customer customer, boolean hasFound, Sort by);
     Page<Lost> findAllByHasFound(boolean hasFound, PageRequest of);
+
     Page<Lost> findAllByHasFoundAndNativeCountry(boolean hasFound, String location, PageRequest of);
+
+    long countByHasFound(boolean b);
+
+    List<Lost> findAllByNativeCountryContainingOrCountryOfLostContainingOrExpectedAddressContaining(String search,String lost,String expectedAddress);
+    List<Lost> findAllByNameContainingOrNativeCountryContainingOrCountryOfLostContainingOrExpectedAddressContainingOrPhoneNumberContainingOrCurrentCountryContaining(
+            String search, String search2, String search3, String search4, String search5, String search6);
+    long countByPostedByAndHasFound(Customer customer, boolean hasFound);
+    List<Lost>findAllByNameContaining(String search);
+
+List<Lost> findAllByHasFound(boolean hasFound);
+
+List<Lost> findAllByNameContainingIgnoreCase(String search);
+
+List<Lost> findAllByPostedByAndHasFound(Customer customer, boolean hasFound);
+List<Lost> findAllByNameContainingAndHasFound(String search, boolean hasFound);
 
 }
