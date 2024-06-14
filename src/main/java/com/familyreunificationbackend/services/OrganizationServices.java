6ec677aa1e4a9185a35f44686f43484d5c65b5ff
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-14 19:40:22 +0200

    Organization Services

--- src/main/java/com/familyreunificationbackend/services/OrganizationServices.java
+++ src/main/java/com/familyreunificationbackend/services/OrganizationServices.java
@@ -1,6 +1,7 @@
 package com.familyreunificationbackend.services;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.hibernate.bytecode.enhance.spi.EnhancementException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,20 +13,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.familyreunificationbackend.input.OrganizationInput;
+import com.familyreunificationbackend.model.Customer;
 import com.familyreunificationbackend.model.Organization;
 import com.familyreunificationbackend.model.pagination.OrganizationPage;
 import com.familyreunificationbackend.model.paginationDefinition.PaginationInput;
+import com.familyreunificationbackend.repository.CustomerRepository;
 import com.familyreunificationbackend.repository.OrganizationRepository;
 
 @Service
 public class OrganizationServices {
     @Autowired
     private OrganizationRepository organizationRepository;
-
+    @Autowired
+    private CustomerRepository customerRepository;
     public ResponseEntity<String> saveOrUpdateOrganization(OrganizationInput data) {
         data.setTimeStamp(LocalDateTime.now());
+        Customer customer=customerRepository.findByUsername(data.getCustomerUserName());
+        if(customer==null){
+            return new ResponseEntity<>("Please add a valid User", HttpStatus.BAD_REQUEST);
+        }
         Organization org = organizationRepository.save(new Organization(data.getId(), data.getName(),
-                data.getBase64Logo(), data.getDescription(), data.getAddress()));
+                data.getBase64Logo(), data.getDescription(), data.getAddress(),customer));
         return new ResponseEntity<>(org.getName() + " organization saved successful", HttpStatus.OK);
     }
 
@@ -49,5 +57,9 @@ public class OrganizationServices {
                 .findAll(PageRequest.of(page.getPageNumber(), page.getPageSize(), Sort.by(page.getSort())));
         return new OrganizationPage(pagination.getNumber(), page.getPageSize(), pagination.getTotalElements(),
                 pagination.getContent());
+    }
+
+    public List<Organization> organizationList() {
+        return organizationRepository.findAll();
     }
 }
