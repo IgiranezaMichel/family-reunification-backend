de8d7364b1ccfd8351975b8aa8a7bd713900df92
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-14 19:39:54 +0200

    Customer Services

--- src/main/java/com/familyreunificationbackend/services/CustomerServices.java
+++ src/main/java/com/familyreunificationbackend/services/CustomerServices.java
@@ -1,18 +1,21 @@
 package com.familyreunificationbackend.services;
 
 import java.util.Base64;
+import java.util.List;
 
 import org.hibernate.bytecode.enhance.spi.EnhancementException;
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
+import com.familyreunificationbackend.Mapper.ChatListMapper;
 import com.familyreunificationbackend.dto.ChangePasswordDTO;
+import com.familyreunificationbackend.dto.ChatListDTO;
 import com.familyreunificationbackend.enums.Role;
 import com.familyreunificationbackend.input.CustomerInput;
 import com.familyreunificationbackend.model.Customer;
@@ -20,32 +23,32 @@ import com.familyreunificationbackend.model.pagination.CustomerPage;
 import com.familyreunificationbackend.model.paginationDefinition.PaginationInput;
 import com.familyreunificationbackend.repository.CustomerRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class CustomerServices {
     @Autowired
     private CustomerRepository userRepository;
-
+ private ChatListMapper chatListMapper=new ChatListMapper();
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public ResponseEntity<String> saveOrUpdateCustomer(CustomerInput userInput) {
         try {
             if (!userInput.getBase64ProfilePicture().contains("base64,"))
-                throw new Exception("Profile Picture is required");
+                return new ResponseEntity("Profile Picture is required",HttpStatus.BAD_REQUEST);
             String imgs = userInput.getBase64ProfilePicture().split("base64,")[1];
             byte[] arr = Base64.getDecoder().decode(imgs);
             Customer user = new Customer(userInput.getId(), userInput.getFirstName(), userInput.getLastName(), arr,
                     userInput.getGender(), userInput.getEmail(), userInput.getPhoneNumber(), userInput.getDob(),
-                    userInput.getAddress(), userInput.getCountry(), userInput.getNativeCountry(), Role.USER,
-                    userInput.getUsername(), userInput.getPassword());
+                    userInput.getAddress(), userInput.getCountry(), userInput.getNativeCountry(),userInput.getRole(),
+                    userInput.getUsername(), BCrypt.hashpw(userInput.getPassword(), BCrypt.gensalt()));
             Customer result = userRepository.save(user);
             if (userInput.getId() != 0)
-                return new ResponseEntity<>("Hi " + result.getFirstName() + " Your Information has updated successful",
-                        HttpStatus.OK);
-            return new ResponseEntity<>("Hi " + result.getFirstName() + " Your Information has saved successful",
-                    HttpStatus.OK);
+                return new ResponseEntity<>("Hi " + result.getFirstName() + " Your Information has updated successful",HttpStatus.OK);
+            return new ResponseEntity<>(result.getUsername()+"#" + result.getFirstName()+"Information has saved successful", HttpStatus.OK);
         } catch (Exception exp) {
-            if (exp instanceof ConstraintViolationException) {
-                return new ResponseEntity<>("User Already exist", HttpStatus.METHOD_NOT_ALLOWED);
-            }
-            return new ResponseEntity<>("Incorrect information or Wrong Chosen Picture", HttpStatus.METHOD_NOT_ALLOWED);
+            log.info("Exception {}",exp.getMessage());
+            return new ResponseEntity<>("User already exist", HttpStatus.METHOD_NOT_ALLOWED);
         }
     }
 
@@ -75,11 +78,25 @@ public class CustomerServices {
     public ResponseEntity<String> changeCustomerPassword(ChangePasswordDTO changePassword) {
         try {
             Customer customer=this.findByUsername(changePassword.getUsername());
-            customer.setPassword(changePassword.getNewPassword());
+            customer.setPassword(BCrypt.hashpw(changePassword.getNewPassword(), BCrypt.gensalt()));
             userRepository.save(customer);
-            return new ResponseEntity<>(customer.getFirstName()+" "+customer.getLastName()+" password changed successful",HttpStatus.ACCEPTED);
+            return new ResponseEntity<>("Hi "+customer.getFirstName()+" "+customer.getLastName()+" Your password changed successful",HttpStatus.ACCEPTED);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(),HttpStatus.METHOD_NOT_ALLOWED);
         }
+    }
+
+    public ResponseEntity<String> updateCustomerRole(long id, Role role) {
+       try {
+         Customer customer=this.findCustomerById(id);
+         customer.setRole(role);
+         Customer customer2=userRepository.save(customer);
+         return new ResponseEntity<>(customer2.getFirstName()+", "+"role saved successful",HttpStatus.OK);
+       } catch (Exception e) {
+        return new ResponseEntity<>("Something went wrong",HttpStatus.BAD_REQUEST);
+       }
+    }
+    public List<ChatListDTO>getAllCutomerToChatList(){
+        return userRepository.findAll().stream().map(chatListMapper).toList();
     }
 }
