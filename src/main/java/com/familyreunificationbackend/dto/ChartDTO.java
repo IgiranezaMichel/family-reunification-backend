ac18306fa2887584012d807da0078c819b0f9af2
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-16 19:34:41 +0200

    chat dto

---
+++ src/main/java/com/familyreunificationbackend/dto/ChartDTO.java
@@ -0,0 +1,11 @@
+package com.familyreunificationbackend.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChartDTO {
+private String title;
+private long total;
+}
