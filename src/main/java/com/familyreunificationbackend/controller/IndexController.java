7d89679cf4136b504bb8e433a9450c589c3da2e2
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-16 19:33:40 +0200

    add customer services

--- src/main/java/com/familyreunificationbackend/controller/IndexController.java
+++ src/main/java/com/familyreunificationbackend/controller/IndexController.java
@@ -1,36 +1,60 @@
 package com.familyreunificationbackend.controller;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.familyreunificationbackend.dto.CustomerDTO;
+import com.familyreunificationbackend.model.Customer;
+import com.familyreunificationbackend.services.CustomerServices;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.Cookie;
+import lombok.extern.slf4j.Slf4j;
+import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
 
-import com.familyreunificationbackend.model.Customer;
-import com.familyreunificationbackend.services.CustomerServices;
-
-import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 public class IndexController {
-
-    @Autowired
-    private CustomerServices customerServices;
-
-    @PostMapping(value = "/login")
-    public ResponseEntity<Customer> customerLogin(@RequestParam(name = "username",required = true) String userName) {
-        try {
-            Customer customer = customerServices.findByUsername(userName);
-            log.info("customer",customer.getFirstName());
-            return new ResponseEntity<>(customer, HttpStatus.OK);
-        }  catch (Exception e) {
-            log.info("error", e);
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+    @Autowired private CustomerServices customerServices;
+    @RequestMapping("/login")
+    public String requestMethodName(HttpServletRequest rServletRequest, HttpServletResponse response,
+            HttpSession session) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        log.info("servlet request {}", rServletRequest.getAuthType());
+        log.info("authentication {}", auth.isAuthenticated());
+        log.info("Email {}", auth.getName());
+        log.info("user detail {}", auth.getDetails());
+        log.info("session {}", rServletRequest.getSession().getId());
+        if (!auth.getName().equals("anonymous")) {
+            Cookie cookie = new Cookie("home", "null");
+            response.addCookie(cookie);
         }
+
+        return new String();
     }
-    @RequestMapping(value = "/handle")
- public ResponseEntity<String> handle(@RequestParam(value = "name") String name, @RequestParam(value = "email") String email) {
-   return new ResponseEntity<String>("Hello World "+name+" "+email, HttpStatus.CREATED);
- }
+
+    @PostMapping(value = "/success-login")
+    public ResponseEntity<CustomerDTO> getMethodName(Principal principal, HttpServletRequest session) {
+        Customer customer = customerServices.findByUsername(principal.getName());
+        log.info("Session generated=", session.getSession().getId());
+        log.info("Session generated=", session.getSession().getId());
+        log.info("User name =", principal.getName());
+        CustomerDTO customerDto=new CustomerDTO(customer.getId(), customer.getFirstName(), customer.getLastName(), null, customer.getGender(), customer.getEmail(), customer.getPhoneNumber(), customer.getAddress(), customer.getCountry(), customer.getNativeCountry(), customer.getRole(), customer.getUsername());
+        return new ResponseEntity<>(customerDto,HttpStatus.OK);
+
+}
+    @GetMapping("/checkSession")
+    public String checkSession(HttpSession session) {
+        if (session != null && session.getAttribute("JSESSIONID") != null) {
+            return "Session is active";
+        }
+        return "Session is inactive";
+    }
+
 }
