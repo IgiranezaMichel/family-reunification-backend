3f3dccba35f0edad3b207c5d79d5c363bdf6127f
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-16 19:38:20 +0200

    getNumberOfCasesPerCaseCategory

--- src/main/java/com/familyreunificationbackend/repository/CasesRepository.java
+++ src/main/java/com/familyreunificationbackend/repository/CasesRepository.java
@@ -1,9 +1,15 @@
 package com.familyreunificationbackend.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.familyreunificationbackend.dto.ChartDTO;
 import com.familyreunificationbackend.model.Cases;
 public interface CasesRepository extends JpaRepository<Cases,Long>{
 
     boolean existsByTitle(String title);
 
+    @Query("SELECT new com.familyreunificationbackend.dto.ChartDTO(c.title,COUNT(a.cases)) FROM Cases c,Lost a   where c.id=a.cases.id  GROUP BY a.cases,c.id")
+    List<ChartDTO>getNumberOfCasesPerCaseCategory();
 }
